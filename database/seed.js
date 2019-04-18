59b584ab35e0ee0f3f35579ee27cd84f2b8b59ed
kevinphm <kevhphm@gmail.com>
2019-04-18 11:16:10 -0700

    Add Company Generation Function

--- database/seed.js
+++ database/seed.js
@@ -1,24 +1,38 @@
-const db  = require('./index.js');
-const StockChart = require('./StockChart.js');
+// const db  = require('./index.js');
+// const StockChart = require('./StockChart.js');
 const generateData = require('./generateData.js');
 
 const stockData = [];
-for (let i = 0; i < 100; i++) {
+
+for (let i = 0; i < 10000000; i++) {
   stockData.push(generateData.generateData(i));
 }
 
-const insertStockData = function() {
-  StockChart.init()
-  .then(() => {
-    StockChart.create(stockData)
-      .then(() => db.close())
-      .catch((e) => {
-        console.log('\n \n \n THERE WAS AN ERROR IN THE DATABASE: \n \n \n', e.message);
-      })
-      .then(() => db.close());
-  })
-  
-  
-};
+console.log(stockData);
 
-insertStockData();
+// var fs = require('fs');
+// var writeStream = fs.createWriteStream('test.txt', { flags : 'w' });
+// var readStream = new MyReadStream();
+
+// readStream.pipe(writeStream);
+// writeStream.on('close', function () {
+//     console.log('All done!');
+// });
+
+
+
+// const insertStockData = function() {
+//   StockChart.init()
+//   .then(() => {
+//     StockChart.create(stockData)
+//       .then(() => db.close())
+//       .catch((e) => {
+//         console.log('\n \n \n THERE WAS AN ERROR IN THE DATABASE: \n \n \n', e.message);
+//       })
+//       .then(() => db.close());
+//   })
+  
+  
+// };
+
+// insertStockData();

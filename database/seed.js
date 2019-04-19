edf0aa9240336a3f5628b997b1138aa6763c04be
kevinphm <kevhphm@gmail.com>
2019-04-18 19:18:26 -0700

    Implement Partial Company Generator

--- database/seed.js
+++ database/seed.js
@@ -1,14 +1,53 @@
 // const db  = require('./index.js');
 // const StockChart = require('./StockChart.js');
 const generateData = require('./generateData.js');
+const file = require("fs").createWriteStream("./test7.dat");
 
 const stockData = [];
 
-for (let i = 0; i < 10000000; i++) {
-  stockData.push(generateData.generateData(i));
+(async() => {
+console.time('generate');
+for (let i = 0; i < 100; i++) {
+//   stockData.push(generateData.generateData(i));
+// }
+if (i === 0){
+  file.write('[ ');
+}
+if(!file.write(JSON.stringify(generateData.generateData(i)))) {
+  // Will pause every 16384 iterations until `drain` is emitted
+  await new Promise(resolve => file.once('drain', resolve));
+}
+if (i < 9999999){
+  file.write(', ')
+} else {
+  file.write(' ]');
+}
 }
 
-console.log(stockData);
+})();
+console.timeEnd('generate');
+
+// for (let i = 0; i < 10000000; i++) {
+//     stockData.push(generateData.generateData(i));
+//   }
+
+//   console.log(stockData.length);
+
+// console.log(stockData);
+
+// var fs = require('fs');
+// var readableStream = fs.createReadStream('file.txt');
+// var data = '';
+
+// readableStream.setEncoding('utf8');
+
+// readableStream.on('data', function(chunk) {
+//     data+=chunk;
+// });
+
+// readableStream.on('end', function() {
+//     console.log(data);
+// });
 
 // var fs = require('fs');
 // var writeStream = fs.createWriteStream('test.txt', { flags : 'w' });

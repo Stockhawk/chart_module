84976b293f234f9c327dec95d3c142d6471417a2
kevinphm <kevhphm@gmail.com>
2019-04-19 21:12:03 -0700

    Update Seed for Deliverable

---
+++ database/cassandra.js
@@ -0,0 +1,38 @@
+var ExpressCassandra = require('express-cassandra');
+var models = ExpressCassandra.createClient({
+    clientOptions: {
+        contactPoints: ['127.0.0.1'],
+        protocolOptions: { port: 9042 },
+        keyspace: 'mykeyspace',
+        queryOptions: {consistency: ExpressCassandra.consistencies.one}
+    },
+    ormOptions: {
+        defaultReplicationStrategy : {
+            class: 'SimpleStrategy',
+            replication_factor: 1
+        },
+        migration: 'safe',
+    }
+});
+
+var MyModel = models.loadSchema('Person', {
+    fields:{
+        name    : "text",
+        surname : "text",
+        age     : "int",
+        created : "timestamp"
+    },
+    key:["name"]
+});
+
+// MyModel or models.instance.Person can now be used as the model instance
+console.log(models.instance.Person === MyModel);
+
+// sync the schema definition with the cassandra database table
+// if the schema has not changed, the callback will fire immediately
+// otherwise express-cassandra will try to migrate the schema and fire the callback afterwards
+MyModel.syncDB(function(err, result) {
+    if (err) throw err;
+    // result == true if any database schema was updated
+    // result == false if no schema change was detected in your models
+});
